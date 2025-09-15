c0a3b10f66cf09e8211fbea06b31853db0d8e2e8
wangqing12 <wangqing12@xcmg>
2025-09-15 17:24:13 +0800

    first commit

---
+++ main/src/components/TheWelcome.vue
@@ -0,0 +1,18 @@
+<script setup>
+import WelcomeItem from './WelcomeItem.vue'
+import DocumentationIcon from './icons/IconDocumentation.vue'
+import ToolingIcon from './icons/IconTooling.vue'
+import EcosystemIcon from './icons/IconEcosystem.vue'
+import CommunityIcon from './icons/IconCommunity.vue'
+import SupportIcon from './icons/IconSupport.vue'
+
+const openReadmeInEditor = () => fetch('/__open-in-editor?file=README.md')
+
+</script>
+
+<template>
+  <WelcomeItem>
+    <WujieVue width="100%" height="100%" name="react-demo" :url="apps['react-demo'].url" :props="{apps}"></WujieVue>
+  </WelcomeItem>
+    
+</template>
---
+++ main/src/main.js
@@ -0,0 +1,26 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import WuJieVue , { preloadApp } from 'wujie-vue3'
+
+let apps = {}
+
+async function loadConfig() {
+    apps = await fetch('/micro-apps.json', {cache: 'no-store'}).then(r=>r.json())
+}
+
+await loadConfig()
+
+
+preloadApp({
+    name: 'vue-demo',
+    url: apps['vue-demo'].url
+})
+
+preloadApp({
+    name: 'react-demo',
+    url: apps['react-demo'].url
+})
+
+createApp(App).use(WuJieVue).mount('#app')
